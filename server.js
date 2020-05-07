be1b2af4735d6d7c9e6e03841abf4d6daa27ed37
Femi Fagbure <femifagbure@gmail.com>
2020-05-07 18:25:26 -0400

    added routes for homepage

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require("express");
-const APIroutes = require("./routes/APIroutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+// const APIroutes = require("./routes/APIroutes");
+const htmlRoutes = require("./routes/indexview");
 
 
 const app = express();
@@ -11,10 +11,17 @@ const port = process.env.PORT || 3000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use("/api", APIroutes);
+//app.use("/api", APIroutes);
 app.use("/", htmlRoutes);
 
 
 app.listen(port, function () {
     console.log(`Listening on PORT: ${port}`)
 });
+
+
+// Routes:
+// Get (.find) route for the workout lists
+// Update (.findByIdAndUpdate method) route to update the workout info 
+// Post (.create method )route to create new workouts 
+// Delete (.findByIdAndDelete(body.id) method) route to delete the workout
